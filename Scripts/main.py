b0c97ddbb63b42f8c953d68bd2e47e7a3934ae35
zanadukhan <cchiu231998@hotmail.ca>
2024-12-29 12:00:04 -0800

    Update .gitignore to include additional file types, enhance README with detailed project information, and add a note in main.py regarding tokenizer settings

--- Scripts/main.py
+++ Scripts/main.py
@@ -20,3 +20,4 @@ if __name__ == '__main__':
 
     drive = DriveUploader(article.title)
     drive.upload_file()
+Make sure to change tokenizer settings and increase 250 character limit for en in TTS/tts/layers/xtts/tokenizer.py
